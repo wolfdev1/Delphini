3df2a198c79e3fa9ea4958d5b8cc0aa81213eda4
wolfdev1 <91499295+wolfdev1@users.noreply.github.com>
2022-02-20 21:54:44 +0100

    Fixed all warnings

--- src/main/java/wolfdev1/com/github/Delphini/Events/Discord/SendMessageOnStreamStart.java
+++ src/main/java/wolfdev1/com/github/Delphini/Events/Discord/SendMessageOnStreamStart.java
@@ -15,21 +15,19 @@ public class SendMessageOnStreamStart {
         eventHandler.onEvent(ChannelGoLiveEvent.class, this::onChannelGoLive);
     }
     public void onChannelGoLive(ChannelGoLiveEvent event) {
-        if(!event.getChannel().getId().equals(Config.CHANNEL)) {
-            return;
-        }else{
             Delphini.jda
                     .getPresence().setActivity
                             (Activity.streaming("Just Chatting", "https://twitch.tv/" + Config.CHANNEL));
             EmbedBuilder eb = new EmbedBuilder()
-                    .setAuthor(event.getChannel().getId(), "https://twitch.tv/" + (event.getChannel().getId()),
+                    .setAuthor(event.getChannel().getName(), "https://twitch.tv/" + (event.getChannel().getId()),
                             "https://hipertextual.com/wp-content/uploads/2021/10/twitch.jpeg")
                     .setTitle(event.getChannel().getId() + " is now streaming **" + event.getStream().getGameName() + "**")
                     .setImage(event.getStream().getThumbnailUrl())
-                    .setFooter("https://twitch.tv/" + (event.getChannel().getId()))
+                    .setFooter("https://twitch.tv/" + (event.getChannel().getName()))
                     .setColor(Color.decode("#a759ff"))
                     ;
             Delphini.jda.getTextChannelById(Config.UPDATE_TEXT_CHANNEL_ID).sendMessageEmbeds(eb.build()).queue();
-        }
+            System.out.println("The channel " + Config.CHANNEL + " is now streaming!");
+
     }
 }
