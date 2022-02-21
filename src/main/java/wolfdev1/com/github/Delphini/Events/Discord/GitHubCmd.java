8ae5f8c2c1142a786f69712bf29753f81612debf
wolfdev1 <91499295+wolfdev1@users.noreply.github.com>
2022-02-21 18:44:21 +0100

    Created a command with the GitHub API

---
+++ src/main/java/wolfdev1/com/github/Delphini/Events/Discord/GitHubCmd.java
@@ -0,0 +1,99 @@
+package wolfdev1.com.github.Delphini.Events.Discord;
+
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
+import org.kohsuke.github.GHUser;
+import org.kohsuke.github.GitHub;
+import wolfdev1.com.github.Delphini.main.Config;
+import wolfdev1.com.github.Delphini.main.Delphini;
+import java.awt.*;
+import java.io.IOException;
+import java.time.Instant;
+import java.util.Objects;
+
+public class GitHubCmd extends ListenerAdapter {
+
+    public void onMessageReceived(@NotNull MessageReceivedEvent event) {
+
+        try {
+
+        String[] args = event.getMessage().getContentRaw().split(" ");
+        if(args[0].equalsIgnoreCase(Config.BOT_PREFIX + "github"))
+        {
+
+                    if(!event.getChannel().getId().equals("940697475088547870"))
+                    {
+                        event.getMessage().reply("Sorry, but you only can execute this at <#940697475088547870>").queue();
+                    }
+                    else
+                    {
+                        GitHub github = Delphini.github;
+                        if (args.length < 2)
+                        {
+                            EmbedBuilder error = new EmbedBuilder()
+                                    .setAuthor(event.getJDA().getSelfUser().getName(), null, event.getJDA().getSelfUser().getAvatarUrl())
+                                    .setTitle("Error")
+                                    .setColor(Color.decode("#ff4242"))
+                                    .setDescription("Usage:\n" +
+                                            "`" + Config.BOT_PREFIX + "github wolfdev1`\n" +
+                                            "`GitHub Command`")
+                                    .setTimestamp(Instant.now())
+                                    .setFooter("GitHub Help")
+                                    ;
+                            event.getMessage().replyEmbeds(error.build()).queue();
+                        }
+                        else
+                        {
+                            GHUser ghUser = github.getUser(args[1]);
+                            if(ghUser.getName() == null)
+                            {
+                                event.getMessage().reply("There was no Github user found with the name **" + args[1] + "**").queue();
+                            }
+                            else
+                            {
+                                GHUser user = github.getUser(args[1]);
+
+                                 EmbedBuilder embedBuilder =
+                                         new EmbedBuilder()
+                                        .setAuthor(user.getName(),
+                                                null, user.getAvatarUrl())
+                                        .setTitle(user.getName() + "'s profile.")
+                                        .setDescription(user.getBio())
+                                                 .setThumbnail(user.getAvatarUrl())
+                                        .addField("Followers",
+                                                (user.getFollowersCount() < 1 ?
+                                                        "*This user has no followers*" : String.valueOf(user.getFollowersCount())) , true)
+                                        .addField("Following",
+                                                (user.getFollowingCount() < 1 ?
+                                                        "*This user does not follow anyone*" : String.valueOf(user.getFollowingCount())), true)
+                                        .addField("Repositories",
+                                                (user.getRepositories().size() < 1 ?
+                                                        "*This user has no repositories*" : String.valueOf(user.getRepositories().size())), false)
+                                        .addField("Created at", String.valueOf
+                                                (user.getCreatedAt().toInstant()), true)
+                                        .addField("Twitter",
+                                                (user.getTwitterUsername() == null
+                                                        ? "*This user has not connected a twitter account.*" : user.getTwitterUsername()), true)
+                                                 .addField("Organization(s)",
+                                                         (user.getOrganizations() == null ? "*User is not in a organization*" : String.valueOf(user.getOrganizations().size())),
+                                                         false)
+                                                 .addField("", (user.getPublicGistCount() < 1 ?
+                                                         "*User has no public gist*" : String.valueOf(user.getPublicGistCount())), false)
+                                        .setTimestamp(Instant.now())
+                                        .setFooter("Github")
+                                        .setColor(Objects.requireNonNull
+                                                (event.getMember()).getColor())
+                                        ;
+                                event.getMessage().replyEmbeds(embedBuilder.build()).queue();
+                            }
+                        }
+                    }
+                }
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+}
+
--- src/main/java/wolfdev1/com/github/Delphini/main/Delphini.java
+++ src/main/java/wolfdev1/com/github/Delphini/main/Delphini.java
@@ -7,18 +7,35 @@ import com.github.twitch4j.TwitchClientBuilder;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.utils.ChunkingFilter;
+import org.kohsuke.github.GitHub;
+import org.kohsuke.github.GitHubBuilder;
+import wolfdev1.com.github.Delphini.Events.Discord.GitHubCmd;
 import wolfdev1.com.github.Delphini.Events.Discord.SendMessageOnStreamStart;
 import wolfdev1.com.github.Delphini.Events.Twitch.*;
 
+import javax.security.auth.login.LoginException;
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Delphini {
     public static JDA jda;
-    public static void main(String[] args) {
+    public static GitHub github;
+
+    static {
+        try {
+            github = new GitHubBuilder().withOAuthToken(Secrets.GITHUB_TOKEN).build();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws LoginException {
         try{
+
             OAuth2Credential credential = new OAuth2Credential("twitch", Secrets.TWITCH_BOT_TOKEN);
 
             TwitchClient twitchClient = TwitchClientBuilder.builder()
@@ -32,19 +49,25 @@ public class Delphini {
             twitchClient.getChat().joinChannel("wolf_mc19");
             SimpleEventHandler eventHandler = twitchClient.getEventManager().getEventHandler(SimpleEventHandler.class);
 
-            new MessageLogger(eventHandler);
-            new HelpCommand(eventHandler);
-            new MinecraftCommand(eventHandler);
+            new Message(eventHandler);
+            new Help(eventHandler);
+            new Minecraft(eventHandler);
             new SendMessageOnStreamStart(eventHandler);
-            new TimeoutCommand(eventHandler);
-            new SayCommand(eventHandler);
+            new Timeout(eventHandler);
+            new Say(eventHandler);
 
             jda = JDABuilder.createDefault(Secrets.DISCORD_BOT_TOKEN)
                     .setActivity((Activity.playing("Lunar Client")))
                     .enableIntents(GatewayIntent.GUILD_MEMBERS)
+                    .addEventListeners(new GitHubCmd())
                     .setChunkingFilter(ChunkingFilter.ALL )
+
                     .build()
                     ;
+
+            jda.upsertCommand("example", "This was only an example")
+                    .addOption(OptionType.ATTACHMENT, "files", "Upload your files here!", true).queue();
+            jda.updateCommands().queue();
         }catch (Exception e) {
             System.out.println(
                     DateTimeFormatter.ofPattern("HH:mm:ss:SS").format(LocalDateTime.now())
